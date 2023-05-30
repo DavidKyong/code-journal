755bfb0669c103c51ee74fb615cc2074e790d259
David Kyong <dvdkyong@gmail.com>
2023-05-30 16:33:08 +0000

    Added beforeunload event listener to the window object that serializes the data model as JSON and it stores the JSON in localStorage.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-const inputURL = document.querySelector('#url');
-const image = document.querySelector('img');
+const $inputURL = document.querySelector('#url');
+const $image = document.querySelector('img');
+const $form = document.querySelector('form');
 
-inputURL.addEventListener('input', function (event) {
+$inputURL.addEventListener('input', function (event) {
   event.preventDefault();
-  image.setAttribute('src', event.target.value);
-}
-);
+  $image.setAttribute('src', event.target.value);
+});
+
+
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const formValue = {};
+  formValue += event.target.value;
+
+});
