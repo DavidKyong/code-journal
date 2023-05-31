6955a3410326104f498ed360c3100e3258d8bb81
David Kyong <dvdkyong@gmail.com>
2023-05-31 23:49:21 +0000

    Made a mistake and added all the code from the first feature and pasted them into the second feature. #2

--- js/main.js
+++ js/main.js
@@ -1,43 +1,94 @@
-function renderEntry(entry) {
+const $inputURL = document.querySelector('#url');
+const $image = document.querySelector('img');
+const $form = document.querySelector('form');
 
+$inputURL.addEventListener('input', function (event) {
+  $image.setAttribute('src', event.target.value);
+});
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const $title = $form.elements.pageTitle.value;
+  const $url = $form.elements.photoURL.value;
+  const $message = $form.elements.message.value;
+
+  const formValue = {
+    title: $title,
+    url: $url,
+    notes: $message
+  };
+
+  formValue.entryId = data.nextEntryId;
+  data.nextEntryId++;
+
+  data.entries.unshift(formValue);
+
+  $image.setAttribute('src', '/images/placeholder-image-square.jpg');
+  $form.reset();
+});
+
+function renderEntry(entry) {
   const $li = document.createElement('li');
 
   const $image = document.createElement('img');
-  $image.setAttribute('src', entry.url);
-
-  const $picTitle = document.createElement('h3');
-  $picTitle.textContent = entry.title;
-
-  const $descriptions = document.createElement('div');
-  $descriptions.className = 'descriptions';
-  $descriptions.textContent = entry.notes;
-
+  $image.setAttribute('src', entry.image);
   $li.appendChild($image);
-  $li.appendChild($picTitle);
-  $li.appendChild($descriptions);
+
+  const $title = document.createElement('h3');
+  $title.textContent = entry.title;
+  $li.appendChild($title);
+
+  const $description = document.createElement('p');
+  $description.textContent = entry.notes;
+  $li.appendChild($description);
 
   return $li;
 }
 
-const $ul = document.querySelector('ul');
+document.addEventListener('DOMContentLoaded', function () {
+  const $entryList = document.getElementById('entriesList');
 
-document.addEventListener('DOMContentLoaded', function (event) {
   for (let i = 0; i < data.entries.length; i++) {
-    const newEntry = data.entries[i];
-    const dataEntry = renderEntry(newEntry);
-    $ul.appendChild(dataEntry);
+    const entry = data.entries[i];
+    const entryElement = renderEntry(entry);
+    $entryList.appendChild(entryElement);
   }
   toggleNoEntries();
 });
 
 function toggleNoEntries() {
-  const $entries = document.querySelector('.hidden');
-  if ($entries) {
-    const $entries = $ul.querySelector('li') !== null;
-    if ($entries) {
-      $entries.className = 'hidden';
+  const $noEntriesMessage = document.querySelector('.no-entries-message');
+
+  if ($noEntriesMessage) {
+    if (data.entries.length === 0) {
+      $noEntriesMessage.className = 'no-entries-message';
     } else {
-      $entries.className = 'none';
+      $noEntriesMessage.className = 'hidden';
     }
   }
 }
+function viewSwap(viewName) {
+  const $entryForm = document.querySelector('[data-view="entry-form"]');
+  const $entries = document.querySelector('[data-view="entries"]');
+
+  if (viewName === 'entry-form') {
+    $entryForm.className = '';
+    $entries.className = 'hidden';
+    data.view = 'entry-form';
+  } else if (viewName === 'entries') {
+    $entryForm.className = 'hidden';
+    $entries.className = '';
+    data.view = 'entries';
+  }
+}
+
+const $entriesViewLink = document.querySelector('#entriesViewLink');
+const $newEntryButton = document.querySelector('#newEntryButton');
+
+$entriesViewLink.addEventListener('click', function () {
+  viewSwap('entries');
+});
+
+$newEntryButton.addEventListener('click', function () {
+  viewSwap('entry-form');
+});
